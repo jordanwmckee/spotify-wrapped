2d40e22d1f1bd63298641ecf4db9e4c9611bfe1f
Jordan <jordanmckee@outlook.com>
2022-12-04 00:30:00 -0500

    top artists

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,6 +2,7 @@ import useFetch from "./useFetch";
 
 const Dashboard = () => {
 	const { data: user, isPending, error } = useFetch('/me');
+	const { data: top, isPending: isPending2, error: error2 } = useFetch('/me/top/artists');
 
 	return ( 
 		<div className="title">
@@ -9,6 +10,16 @@ const Dashboard = () => {
 			{ error && <div>{ error }</div> }
 			{ isPending && <p>Fetching User Info...</p> }
 			{ user && <p> Hello, { user && user.display_name }</p> }
+			{ top &&
+				<div className="top-artists">
+					{ top.items.map((t) => (
+						<div className="artist" key={t.id}>
+							<h2>{ t.name }</h2>
+							<img src={t.images[0].url} width="100px" height="100px" alt="No image available." />
+						</div>
+					))}
+				</div>
+			}
 		</div>
 	);
 }
