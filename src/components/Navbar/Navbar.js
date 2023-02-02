db0f115e751359d8da9477c2c9ae7ef6894100bd
Andrew Markland <Crecentbear@gmail.com>
2023-02-01 22:37:36 -0500

    add logo to dashboard and style change anylytics tab and add graph example model

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 import { logout } from '../../Firebase';
 import './Navbar.css';
+//import '../../pics&logos/SW_logo.png';
 
 const Navbar = () => {
 	return ( 
 		<nav className="navbar">
-			<h1>Wrapped Monthly</h1>
+			
+			<img className='logo' src={require('../../pics&logos/SW_logo.png')} alt='ber_logo'></img>
 			<div className="links">
 				<Link to="/dashboard">Dashboard</Link>
 				<Link to="/analytics">Analytics</Link>
