7c2f00257d65fc6fd46d005595c30ba9762a7569
Jordan <jordanmckee@outlook.com>
2023-02-02 19:43:36 -0500

    chore: move logos to assets folder

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,23 @@
-import { Link } from 'react-router-dom';
-import { logout } from '../../firebase';
-import './Navbar.css';
+import { Link } from "react-router-dom";
+import { logout } from "../../firebase";
+import "./Navbar.css";
 //import '../../pics&logos/SW_logo.png';
 
 const Navbar = () => {
-	return ( 
-		<nav className="navbar">
-			
-			<img className='logo' src={require('../../pics&logos/SW_logo.png')} alt='ber_logo'></img>
-			<div className="links">
-				<Link to="/dashboard">Dashboard</Link>
-				<Link to="/analytics">Analytics</Link>
-				<button onClick={logout}>LOGOUT</button>
-			</div>
-		</nav>
-	);
-}
- 
+  return (
+    <nav className="navbar">
+      <img
+        className="logo"
+        src={require("../../assets/logos/logo.png")}
+        alt="ber_logo"
+      />
+      <div className="links">
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/analytics">Analytics</Link>
+        <button onClick={logout}>LOGOUT</button>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
