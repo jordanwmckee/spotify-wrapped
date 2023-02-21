bc66644e4b6ceaf8db9dbcb93c68fef38e8c35d8
Jordan <jordanmckee@outlook.com>
2023-02-21 14:15:22 -0500

    feat: default user profile section if api fetch fails

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { logout, removeUser, unlinkSpotify } from "../../firebase";
+import { logout } from "../../firebase";
 import { spotifyApi } from "../../spotify";
 import "./Navbar.css";
 
@@ -49,6 +49,7 @@ const Navbar = (spotifyLinked) => {
 
   return (
     <div id="navbar">
+      {/* Site Logo */}
       <Link to="/">
         <img
           className="logo"
@@ -56,34 +57,55 @@ const Navbar = (spotifyLinked) => {
           alt="ber_logo"
         />
       </Link>
+      {/* User Profile Dropdown */}
       <div
         className="profile"
         onClick={toggleDropdown}
         onMouseLeave={closeDropdown}
       >
-        {profilePic && (
-          <>
-            <img className="profile-pic" src={profilePic} alt="" />
-            <img
-              className="dd-arrow"
-              src={require("../../assets/images/dd-arrow.png")}
-              alt=""
-            />
-            <div className="profile-dropdown">
-              <div className="dropdown-top">
-                <img src={profilePic} alt="" />
-                {name && <h3>{name}</h3>}
-              </div>
-              <div className="options">
-                <h4>Some Option</h4>
-                <h4>Another Option</h4>
-              </div>
-              <div className="logout">
-                <h4 onClick={logout}>Logout</h4>
-              </div>
-            </div>
-          </>
+        {profilePic ? (
+          <img
+            className="profile-pic"
+            src={profilePic}
+            alt={require("../../assets/images/default-pfp.png")}
+          />
+        ) : (
+          <img
+            className="profile-pic"
+            src={require("../../assets/images/default-pfp.png")}
+            alt=""
+          />
         )}
+        <img
+          className="dd-arrow"
+          src={require("../../assets/images/dd-arrow.png")}
+          alt=""
+        />
+        <div className="profile-dropdown">
+          <div className="dropdown-top">
+            {profilePic && name ? (
+              <>
+                <img src={profilePic} alt="" />
+                <h3>{name}</h3>
+              </>
+            ) : (
+              <>
+                <img
+                  src={require("../../assets/images/default-pfp.png")}
+                  alt=""
+                />
+                <h3>User</h3>
+              </>
+            )}
+          </div>
+          <div className="options">
+            <h4>Some Option</h4>
+            <h4>Another Option</h4>
+          </div>
+          <div className="logout">
+            <h4 onClick={logout}>Logout</h4>
+          </div>
+        </div>
       </div>
     </div>
   );
