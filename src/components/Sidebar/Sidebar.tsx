8bad9e5e731335548039457fca764dc3b643e919
Jordan <jordanmckee@outlook.com>
2023-03-19 21:44:30 -0400

    feat: simple sidebar styling

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import DashboardIcon from "assets/logos/dashboard.png";
+import AnalyticsIcon from "assets/logos/analytics.png";
+import CreateIcon from "assets/logos/forYou.png";
 import "./Sidebar.css";
 
 const Sidebar = () => {
@@ -8,17 +11,24 @@ const Sidebar = () => {
         <h3>Navigation</h3>
       </div>
       <div className="links">
-        <Link to="/" className="link">
-          Dashboard
-        </Link>
-        <br />
-        <Link to="/analytics" className="link">
-          Analytics
-        </Link>
-        <br />
-        <Link to="/" className="link">
-          Create
-        </Link>
+        <div className="sidebar-item">
+          <img src={DashboardIcon} alt="" />
+          <Link to="/" className="link">
+            Dashboard
+          </Link>
+        </div>
+        <div className="sidebar-item">
+          <img src={AnalyticsIcon} alt="" />
+          <Link to="/analytics" className="link">
+            Analytics
+          </Link>
+        </div>
+        <div className="sidebar-item">
+          <img src={CreateIcon} alt="" />
+          <Link to="/" className="link">
+            Create
+          </Link>
+        </div>
       </div>
     </div>
   );
