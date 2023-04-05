d27b632ef3ab1dfb1527cd7e6025d96d38fc31ce
Jordan <jordanmckee@outlook.com>
2023-04-04 21:31:35 -0400

    feat: improve app.tsx functionality by running api fetches concurrently

--- src/App.tsx
+++ src/App.tsx
@@ -55,107 +55,103 @@ function App() {
   );
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    let tokenDetected: boolean = true;
+  useEffect(
+    () => {
+      // create a variable to store a cleanup function
+      let unmounted = false;
 
-    // get & update all spotify info to be store in the app
-    // (thank god for reconciliation)
-    const getData = async () => {
-      // validate access token
-      await refreshAuthToken();
-      // get recommend songs and uris for player
-      if (!recommendUris || !recommendedSongs) {
-        const { urisArr, tracksArr } = await getRecommendedTracks();
-        dispatch(SET_RECOMMEND_URIS(urisArr));
-        setRecommendedSongs(tracksArr);
-      }
-      if (!displayName || !profilePic) {
-        // get user spotify account
-        const userAccount = await spotifyApi.getMe();
-        setDisplayName(userAccount.display_name || '');
-        setProfilePic(userAccount.images![0].url || '');
-      }
-      if (!monthlySongs || !monthlyArtists) {
-        // get top monthly user items
-        const { topTracks: monthlySongs, topArtists: monthlyArtists } =
-          await getTopItems({ time_range: 'short_term', limit: '16' });
-        setMonthlySongs(monthlySongs);
-        setMonthlyArtists(monthlyArtists);
-        const recommendedArtists = await getRecommendedArtists(
-          monthlyArtists[0].id!
-        );
-        setRecommendedArtists(recommendedArtists);
-      }
-      if (!allTimeSongs || !allTimeArtists) {
-        // get top all time items
-        const { topTracks: allTimeSongs, topArtists: allTimeArtists } =
-          await getTopItems({ time_range: 'long_term', limit: '16' });
-        setAllTimeSongs(allTimeSongs);
-        setAllTimeArtists(allTimeArtists);
-      }
-      if (!recentListens || !recentGenres) {
-        //get listen history
-        const { listenHistory: recentListens, genresArr: genresList } =
-          await getRecentListens({ limit: 50 });
-        setRecentListens(recentListens);
-        setRecentGenres(genresList);
-      }
-      if (!monthlyListens || !monthlyGenres) {
-        //get top monthly tracks & genres
-        const { topMonthly: monthlyListens, TopMonthGenres: monthlyGenres } =
-          await getMonthlyListens({ time_range: 'short_term', limit: 50 });
-        setMonthlyListens(monthlyListens);
-        setMonthlyGenres(monthlyGenres);
-      }
-      if (!allTimeListens || !allTimeGenres) {
-        //get top Alltime tracks & genres
-        const { allTListens: allTimeListens, allTGenres: allTimeGenres } =
-          await getAlltimeListens({ time_range: 'long_term', limit: 50 });
-        setAllTimeListens(allTimeListens);
-        setAllTimeGenres(allTimeGenres);
-      }
-      // get user playlists for sidebar popup
-      !userPlaylists && setUserPlaylists(await getUserPlaylists());
-    };
+      // wrap the async code inside a function
+      const getData = async () => {
+        // refresh auth token before making requests
+        await refreshAuthToken();
+        // perform all the async operations in parallel
+        let recommendedArtistsResult: RecommendedItems[] = [];
+        const [
+          recommendedTracksResult,
+          userAccountResult,
+          topMonthlyResult,
+          topAllTimeResult,
+          listenHistoryResult,
+          topMonthlyGenresResult,
+          topAllTimeGenresResult,
+          userPlaylistsResult,
+        ] = await Promise.all([
+          getRecommendedTracks(),
+          spotifyApi.getMe(),
+          getTopItems({ time_range: 'short_term', limit: '16' }),
+          getTopItems({ time_range: 'long_term', limit: '16' }),
+          getRecentListens({ limit: 50 }),
+          getMonthlyListens({ time_range: 'short_term', limit: 50 }),
+          getAlltimeListens({ time_range: 'long_term', limit: 50 }),
+          getUserPlaylists(),
+        ]).then(async (results) => {
+          recommendedArtistsResult = await getRecommendedArtists(
+            monthlyArtists![0].id!
+          );
+          return results;
+        });
 
-    // check for valid access token and update linked bool
-    const checkToken = async () => {
-      let isLinked: boolean = checkForTokens();
-      // try to get tokens if spotify is not linked to user
-      if (isLinked === false && window.location.search.includes('code')) {
-        await fetchTokensFromCode();
-        isLinked = true;
-      }
-      if (isLinked) tokenDetected = true;
-      else tokenDetected = false;
-      setLinked(isLinked);
-    };
+        // update state with the results
+        if (!unmounted) {
+          dispatch(SET_RECOMMEND_URIS(recommendedTracksResult.urisArr));
+          setRecommendedSongs(recommendedTracksResult.tracksArr);
 
-    setLoading(true);
-    checkToken();
-    if (tokenDetected) getData();
-    setLoading(false);
-    if (!playerUris && recommendUris) dispatch(SET_PLAYER_URIS(recommendUris));
-  }, [
-    loading,
-    recommendUris,
-    playerUris,
-    displayName,
-    profilePic,
-    monthlyArtists,
-    monthlySongs,
-    allTimeArtists,
-    allTimeSongs,
-    recentListens,
-    recentGenres,
-    allTimeListens,
-    allTimeGenres,
-    monthlyListens,
-    monthlyGenres,
-    userPlaylists,
-    recommendedArtists,
-    recommendedSongs,
-  ]);
+          setDisplayName(userAccountResult.display_name || '');
+          setProfilePic(userAccountResult.images![0].url || '');
+
+          setMonthlySongs(topMonthlyResult.topTracks);
+          setMonthlyArtists(topMonthlyResult.topArtists);
+          setRecommendedArtists(recommendedArtistsResult);
+
+          setAllTimeSongs(topAllTimeResult.topTracks);
+          setAllTimeArtists(topAllTimeResult.topArtists);
+
+          setRecentListens(listenHistoryResult.listenHistory);
+          setRecentGenres(listenHistoryResult.genresArr);
+
+          setMonthlyListens(topMonthlyGenresResult.topMonthly);
+          setMonthlyGenres(topMonthlyGenresResult.TopMonthGenres);
+
+          setAllTimeListens(topAllTimeGenresResult.allTListens);
+          setAllTimeGenres(topAllTimeGenresResult.allTGenres);
+
+          setUserPlaylists(userPlaylistsResult);
+
+          if (!playerUris && recommendedTracksResult.urisArr) {
+            dispatch(SET_PLAYER_URIS(recommendedTracksResult.urisArr));
+          }
+        }
+        setLoading(false);
+      };
+
+      const checkToken = async () => {
+        let isLinked = checkForTokens();
+        if (isLinked === false && window.location.search.includes('code')) {
+          await fetchTokensFromCode();
+          isLinked = true;
+        }
+        if (!unmounted) {
+          setLinked(isLinked);
+        }
+      };
+
+      setLoading(true);
+      checkToken();
+      getData();
+
+      // cleanup function to set unmounted flag
+      return () => {
+        unmounted = true;
+      };
+    },
+    [
+      // playerUris,
+      // displayName,
+      // profilePic,
+      // recommendedSongs,
+      // recommendUris,
+    ]
+  );
 
   return (
     <div className="App">
